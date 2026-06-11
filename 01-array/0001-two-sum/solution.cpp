f2a31ca9833cfdf995efceb441ed765bfa234c3d
h72545710 <h72545710@gmail.com>
2026-06-11 17:09:44 +0800

    update 0001 two sum note

---
+++ 01-array/0001-two-sum/solution.cpp
@@ -0,0 +1,22 @@
+#include <vector>
+#include <unordered_map>
+using namespace std;
+
+class Solution {
+public:
+    vector<int> twoSum(vector<int>& nums, int target) {
+        unordered_map<int, int> hash;
+
+        for (int i = 0; i < nums.size(); i++) {
+            int need = target - nums[i];
+
+            if (hash.count(need)) {
+                return {hash[need], i};
+            }
+
+            hash[nums[i]] = i;
+        }
+
+        return {};
+    }
+};
---
+++ README.md
@@ -0,0 +1,3 @@
+![Alt text](image.png)
+
+提交代码方式如上图所示
